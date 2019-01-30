13032a49039f5424b0a3deb33d139828b11585f1
Sam Mason <smason@cloudflare.com>
2019-01-30 13:30:28 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+require("dotenv").config()
+const shuffle = require("lodash/shuffle")
+const chunk = require("lodash/chunk")
+const got = require("got")
+
+const webhookUrl = process.env.WEBHOOK_URL
+
+// Add more people here
+const people = [
+  // Sam Mason
+  {
+    id: "115239313547085426897"
+  },
+  // Susan Lin
+  {
+    id: "101959280054230313273"
+  }
+]
+
+const mainMessage = `*It's critique time!*
+Organize a session with your selected peers this week. Bring Work in Progress at any stage for critique. Refer back to our deck for best practices <https://docs.google.com/presentation/d/1Pc7K-buiXnT2pTzAEbzRTQRJapO97CLNWBBPldpt5KQ/edit?usp=sharing|here>\n
+`
+
+const genGroups = () => {
+  const groups = chunk(shuffle(people), 3)
+  return groups.map((group, i) => {
+    return `*Group #${i + 1}*
+${group.map(p => ` <users/${p.id}>`)}`
+  })
+}
+
+const postMessage = async () => {
+  try {
+    await got(webhookUrl, {
+      method: "POST",
+      body: { text: `${mainMessage}${genGroups()}` },
+      json: true
+    })
+    return Promise.resolve("Success")
+  } catch (error) {
+    return Promise.reject(`Something went wrong ${error}`)
+  }
+}
+
+module.exports = async (req, res) => {
+  try {
+    await postMessage()
+    return res.end("success")
+  } catch (error) {
+    res.statusCode = 400
+    return res.end()
+  }
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Critique
+
+A Google Chat bot that every week will randomly group the various design team members into groups of 3 and alert them to organise a crituqe session to openly discuss current works in progress and train our critique muscles.
+
+## Contributing
+
+If you're a new member to the design team, we would love for you to take part in these critique sessions. We would also love you to be able to contribute to our codebase and add yourself to this tool. Follow the instreuctions below on how to get started.
+
+### Adding yourself
+
+- Send a message to Sam Mason to ask for your Google chat user id
+- Go to the main.js file
+- Click on the edit button and on line 9 you should see something like this
+
+```
+const people = [
+  ...
+]
+```
+
+- Just before the `]` character copy and paster the following code with the user id you got from Sam earlier
+
+```javascript
+// Your name here
+{
+  id: "Your user id goes here"
+}
+```
+
+- At the bottom of the page there is a box that says "Commit changes", add in a descriptive message about what change you have made e.g Added my id to the list of people
+- Then click the propose new chnage, this creates a Pull Request that will give the maintainers an opportunity to review the code change before merging it into the master branch.
